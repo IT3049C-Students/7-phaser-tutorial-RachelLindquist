3d168b74e764bab09a37e6d906144449d784c89c
RachelLindquist <lindqurm@mail.uc.edu>
2022-10-19 18:48:56 -0400

    video 6 finished

--- Scene2.js
+++ Scene2.js
@@ -4,12 +4,14 @@ class Scene2 extends Phaser.Scene{
     }
 
     create(){
-        this.background = this.add.tileSprite(0,0, config.width, config.height, "background");
+        // https://phaser.discourse.group/t/facing-error-as-uncaught-typeerror-cannot-read-property-config-of-undefined/6574/10
+        // config.width didn't work, had to use this.game before it
+        this.background = this.add.tileSprite(0,0, this.game.config.width, this.game.config.height, "background");
         this.background.setOrigin(0,0);
 
-        this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
-        this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
-        this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
+        this.ship1 = this.add.sprite(this.game.config.width/2 - 50, this.game.config.height/2, "ship");
+        this.ship2 = this.add.sprite(this.game.config.width/2, this.game.config.height/2, "ship2");
+        this.ship3 = this.add.sprite(this.game.config.width/2 + 50, this.game.config.height/2, "ship3");
 
         this.anims.create({
             key: "ship1_anim",
@@ -53,11 +55,51 @@ class Scene2 extends Phaser.Scene{
 
 
         this.add.text(20,20, "Playing game", {font: "25px Arial", fill: "yellow"});
+
+        this.anims.create({
+            key: "red",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start: 0,
+                end: 1
+            }),
+            frameRate:20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "gray",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start: 2,
+                end: 3
+            }),
+            frameRate:20,
+            repeat: -1
+        });
+
+        this.powerUps = this.physics.add.group();
+
+        let maxObjects = 4;
+        for (let i = 0; i < maxObjects; i++){
+            let powerUp = this.physics.add.sprite(16,16, "power-up");
+            this.powerUps.add(powerUp);
+            powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+
+            if (Math.random() > 0.5){
+                powerUp.play("red");
+            } else {
+                powerUp.play("gray");
+            }
+
+            powerUp.setVelocity(100,100);
+            powerUp.setCollideWorldBounds(true);
+            powerUp.setBounce(1);
+
+        }
     }
 
     moveShip(ship, speed){
         ship.y += speed;
-        if (ship.y > config.height){
+        if (ship.y > this.game.config.height){
             this.resetShipPos(ship);
         }
     }
@@ -72,7 +114,7 @@ class Scene2 extends Phaser.Scene{
 
     resetShipPos(ship){
         ship.y = 0;
-        let randomX = Phaser.Math.Between(0, config.width);
+        let randomX = Phaser.Math.Between(0, this.game.config.width);
         ship.x = randomX;
     }
 
--- game.js
+++ game.js
@@ -4,7 +4,13 @@ window.onload = function (){
         height: 272,
         backgroundColor: 0x000000,
         scene: [Scene1, Scene2],
-        pixelArt: true
+        pixelArt: true,
+        physics: {
+            default: "arcade",
+            arcade:{
+                debug: false
+            }
+        }
     }
     let game = new Phaser.Game(config);
 }
